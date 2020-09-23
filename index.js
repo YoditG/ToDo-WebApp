de25dd7472c79c66f513bce0b76b86a3121ec23e
Kene_Okoye <mr.kene.okoye@gmail.com>
2020-09-23 17:34:37 +0200

    Add edit delete functionality

--- index.js
+++ index.js
@@ -20,36 +20,119 @@ const inputField = document.getElementById('inputField');
 // let inputFieldStorage = inputField.value; 
 
 
+let userToDos = [];
 
+const createNewTask = () => {
+  if (inputField.value !== "") {
+    const taskId = Date.now().toString()
+    let userToDosObject = { id: taskId, content: inputField.value, description: "" };
+    userToDos.push(userToDosObject);
 
-    let userToDos= [];
+    // Create a div to encapsulate each task 
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
 
-    const createNewTask = () => {
-      const taskId = Date.now().toString()
-      userToDos.push({id:taskId, content: inputField.value })
-
-     const task = document.createElement('p');
-     task.dataset.taskId = taskId;
+    // Create a pragraph for task names
+    const task = document.createElement('p');
+    task.dataset.taskId = taskId;
     task.classList.add('task-styling');
     task.innerText = inputField.value;
-    toDoContainer.appendChild(task);
-    inputField.value= "";
-    
+    taskContainer.appendChild(task);
+    inputField.value = "";
+
+
+    console.log(userToDos)
+
+    // Create edit and delete buttons
+    const editButton = document.createElement("button");
+    editButton.classList.add("edit-button");
+    editButton.innerText = "Edit";
+
+    // Add event listener for the edit button
+    editButton.addEventListener("click", editTask);
+
+    // function for edit button
+    function editTask() {
+      // Prevent user immediately from triggering edit button event while in edit mode
+      editButton.removeEventListener("click", editTask);
+      // Or use ---> editButton.disabled = true;
+
+      // Make task name content editable
+      task.contentEditable = true;
+
+      // Create a div to encapsulate the text area and the save button
+      const descriptionDiv = document.createElement("div");
+      descriptionDiv.classList.add("description-div");
+
+      // Display text area to add a description 
+      const taskDescriptionTextArea = document.createElement("textarea");
+      taskDescriptionTextArea.classList.add("task-description-text-area");
+
+      taskDescriptionTextArea.value = userToDosObject.description
+
+      // if the length of userTodosObject.descrcription is > 0 then take the existing content of
+      // userTodosObject.descrcription and set the value of the textDescriptionTextArea.value = 
+      // the content of userTodosObject.description
+
+      // Add a save button together with its corresponding eventsto save changes
+      const saveEditsButton = document.createElement("button");
+      saveEditsButton.classList.add("save-edits-button");
+      saveEditsButton.textContent = "Save";
+      saveEditsButton.addEventListener("click", saveChanges);
+
+      function saveChanges(e) {
+        // Make task name content non-editable
+        task.contentEditable = false;
+        e.target.parentNode.remove();
+
+        // Make edit button active after saving
+        editButton.addEventListener("click", editTask);
+        // Or use ---> editButton.disabled = false;
+
+        // Save the user data as objects and push to an array ONLY when user adds a description
+
+
+        userToDosObject.description = taskDescriptionTextArea.value;
+        userToDosObject.content = task.innerText;
+        console.log(userToDosObject.description)
+        console.log(userToDosObject)
+      }
+
+      // Append task description area and save button elements to the DOM
+      descriptionDiv.appendChild(taskDescriptionTextArea);
+      descriptionDiv.appendChild(saveEditsButton);
+      toDoContainer.appendChild(descriptionDiv);
     }
-   
 
-   
-    
-    
-   addTaskButton.addEventListener('click', createNewTask);
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.classList.add("delete-button");
 
-    
+    // Add event listener to delete button
+    deleteButton.addEventListener("click", deleteTask);
+    function deleteTask(e) {
+      e.currentTarget.parentNode.remove();
+    }
+
+    // append the paragraph delete and edit buttons to the div
+    taskContainer.appendChild(editButton);
+    taskContainer.appendChild(deleteButton);
+    toDoContainer.appendChild(taskContainer);
+  } else {
+    alert("Ooops, you can't create an empty task.")
+  }
+}
+
+// Add event listener to create new task
+addTaskButton.addEventListener('click', createNewTask);
+
 inputField.addEventListener('keydown', (event) => {
-        console.log(event.key)
-        if (event.key === 'Enter') {
-          createNewTask()
-        }
-      }); 
+  console.log(event.key)
+  if (event.key === 'Enter') {
+    createNewTask()
+  }
+});
+
 
 //-------------------------- JSON STORAGE SECION -----------------------------------------------
 
